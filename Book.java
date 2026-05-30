50ef88d4e874ca92cd2c11ac6005eb3844d42b1f
Vanshika Bhan <bhanvanshika7@gmail.com>
2026-05-30 15:47:06 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    int id;
+    String title;
+    String author;
+    int quantity;
+
+    public Book(int id, String title, String author, int quantity) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.quantity = quantity;
+    }
+}
